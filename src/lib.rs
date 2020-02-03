1e66d4460c1e9f410edf99165cb93a256409c0cc
Zacck Osiemo <coderv63@gmail.com>
2020-02-03 07:02:46 +0200

    Add a scoped struct with scoped methods

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,27 @@ mod front_of_house {
 }
 
 mod back_of_house {
+    pub struct Breakfast {
+        // toast field is public
+        pub toast: String,
+        // seasonal fruit is private and allows no access or operations on it
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+
+        pub fn what_fruit(&self) {
+            println!("{}", self.seasonal_fruit)
+        }
+    }
+
+    // Call a function in the ancestor using super
     pub fn fix_incorrect_order() {
         cook_order();
         super::serve_order();
@@ -36,4 +57,14 @@ pub fn eat_at_restaurant() {
 
     //Cook something
     back_of_house::fix_incorrect_order();
+
+    // Order breakfast with some sourdough
+    let mut meal = back_of_house::Breakfast::summer("Sourdough");
+
+    // Oh no they have Commo Ciabatta let's get that instead
+    meal.toast = String::from("Commo Ciabatta");
+    println!("I'd like {} toast please", meal.toast);
+
+    // Show the fruit
+    meal.what_fruit();
 }
